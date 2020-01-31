eafffcf4714085224cb244e8abe3b7b0ef5d2260
jamievullo <jvullo@protonmail.com>
2020-01-31 14:55:53 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
 // Code SimpleComponent Here
+import React from 'react';
+
+export default class SimpleComponent extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            mood: 'happy'
+        }
+    }
+
+    handleClick = () => {
+        //had to peek for this simplified ternary...had the rest
+        const updatedMood = this.state.mood === 'happy' ? 'sad' : 'happy'
+        this.setState({mood: updatedMood})
+    }
+
+    render() {
+        return <div onClick={this.handleClick}>{this.state.mood}</div>
+    }
+}

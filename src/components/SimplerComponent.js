9015b60dfa3f8ea92dd07f7b368fdb795e3d9946
Jason McGloin <jason.mcgloin@gmail.com>
2020-02-14 23:06:32 -0500

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,11 @@
-// Code SimplerComponent Here
+import React from 'react';
+
+const SimplerComponent = (props) => {
+	return (
+		<React.Fragment>
+			<div onClick={props.handleClick}>I am just happy</div>
+		</React.Fragment>
+	)
+}
+
+export default SimplerComponent
